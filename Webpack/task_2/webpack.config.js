1773399d58ae9e7fd22ef68ed0843806de63890f
Silvycr <silvia_alinacr@live.com>
2023-11-12 05:43:00 -0800

    Adding CSS & Images

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,28 +1,27 @@
 const path = require('path');
 
 module.exports = {
-	mode: 'production',
-	entry: './js/dashboard_main.js',
-	output: {
-		path: path.resolve(__dirname, 'public'),
-		filename: 'bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				use: [
-				     'file loader',
-				     {
-					loader: 'image-webpack-loader',
-				     },
-				],
-			},
-
-		],
-	};
-}
+    mode: 'production',
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                    }
+                ]
+            },
+        ],
+    },
+};
